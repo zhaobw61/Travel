3aaf16588af055f3a013fd4fd99c5cf5e997d6a8
zhaobw <1964471757@qq.com>
2018-09-16 15:01:02 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 显示的是当前路由所对应内容 -->
     <router-view/>
   </div>
 </template>
 
 <script>
+/**
+ * 以vue结尾的文件叫做单文件组件
+ * 模板放在template
+ * 逻辑放在script文件里
+ * 样式放在style里
+ */
 export default {
   name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        hellow
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
