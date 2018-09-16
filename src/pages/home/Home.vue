7f0d8463aeaf406bde1491663ea499ddeadf5fb5
zhaobw <1964471757@qq.com>
2018-09-16 21:31:15 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
     <div>
       <home-hearder></home-hearder>
       <home-swiper></home-swiper>
+      <home-icon></home-icon>
     </div>
 </template>
 
 <script>
 import HomeHearder from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
+import HomeIcon from './components/Icon.vue'
 export default {
   name: 'Home',
   components: {
     HomeHearder: HomeHearder,
-    HomeSwiper: HomeSwiper
+    HomeSwiper: HomeSwiper,
+    HomeIcon: HomeIcon
   }
 }
 </script>
---
+++ src/pages/home/components/Icon.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-imgcontent" :src=item.imgUrl alt="">
+          </div>
+          <p class='icon-desc'>{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcon',
+  data: function () {
+    return {
+      swiperOption: {
+        loop: true
+      },
+      iconList: [{
+        id: '001',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '003',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '004',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '005',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '006',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '007',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '008',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }, {
+        id: '008',
+        imgUrl: 'https://s.qunarzz.com/package_mobile/app/home/icon-local.png',
+        desc: '景点门票'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varbles.styl'
+@import '~styles/mixins.styl'
+  .icons >>> .swiper-container{
+    height :0;
+    padding-bottom : 50%;
+  }
+  .icon{
+    position relative;
+    overflow :hidden;
+    float: left;
+    width:25%;
+    height 0;
+    padding-bottom:25%;
+    background : #ccc;
+    .icon-umg{
+      position absolute;
+      top: 0;
+      left 0;
+      right 0;
+      bottom .44rem;
+      box-sizing : border-box;
+      padding  .1rem;
+      background blue;
+      .icon-imgcontent{
+        display :block;
+        margin 0 auto;
+        height 100%;
+      }
+    }
+    .icon-desc{
+      position :absolute;
+      left :0;
+      right :0;
+      bottom 0;
+      height: .44rem;
+      line-height :.44rem;
+      text-align :center;
+      color: $darkTextColor;
+      ellipsis();
+    }
+  }
+</style>
