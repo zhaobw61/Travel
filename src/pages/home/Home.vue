d266738f61e9a561f4a1716c16cfaa6e33f6a96d
zhaobw <1964471757@qq.com>
2018-09-17 00:11:37 +0800

    recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
       <home-hearder></home-hearder>
       <home-swiper></home-swiper>
       <home-icon></home-icon>
+      <home-recommend></home-recommend>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeHearder from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcon from './components/Icon.vue'
+import HomeRecommend from './components/Recommend.vue'
 export default {
   name: 'Home',
   components: {
     HomeHearder: HomeHearder,
     HomeSwiper: HomeSwiper,
-    HomeIcon: HomeIcon
+    HomeIcon: HomeIcon,
+    HomeRecommend: HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom"  v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src = item.imgUrl alt="">
+        <div class="item-info">
+          <p class="item-title">上海杜莎夫人蜡像馆</p>
+          <p class="item-desc">上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_200x200_46052773.jpg',
+        title: '上海杜莎夫人蜡像馆',
+        desc: '上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_200x200_46052773.jpg',
+        title: '上海杜莎夫人蜡像馆',
+        desc: '上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_200x200_46052773.jpg',
+        title: '上海杜莎夫人蜡像馆',
+        desc: '上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_200x200_46052773.jpg',
+        title: '上海杜莎夫人蜡像馆',
+        desc: '上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_200x200_46052773.jpg',
+        title: '上海杜莎夫人蜡像馆',
+        desc: '上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1704/c9/c936f3fccfc6d7eda3.img.jpg_200x200_46052773.jpg',
+        title: '上海杜莎夫人蜡像馆',
+        desc: '上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆上海杜莎夫人蜡像馆'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title{
+    margin-top :.2rem;
+    line-height :.8rem;
+    background : #ccc;
+    text-indent: .2rem;
+  }
+  .item{
+    display:flex;
+    overflow hidden;
+    height: 1.9rem;
+    .item-img{
+      width: 1.7rem;
+      height : 1.7rem;
+      padding :.1rem;
+    }
+    .item-info{
+      flex:1;
+      paddind : .1rem;
+      min-width : 0;
+      .item-title{
+        line-height :.54rem;
+        font-size :.32rem;
+        ellipsis();
+      }
+      .item-desc{
+        line-height : .4rem;
+        color : #ccc;
+        ellipsis();
+      }
+      .item-button{
+        line-height : .44rem;
+        margin-top : .16rem;
+        background :#ff9300;
+        padding:0 .2rem;
+        border-radius: .06rem;
+        color :#fff;
+      }
+    }
+  }
+</style>
