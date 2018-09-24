3a2d4ace357ef24b194c8d0ba25c452534997001
zhaobw <1964471757@qq.com>
2018-09-24 17:19:13 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <div class="banner" @click='handleBannerClick'>
+            <img class="banner-img" 
+                src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-tittle">海洋世界(AAAAA景区)</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe692;</span>
+                    39
+                </div>
+            </div>
+        </div>
+        <common-gallary
+            @close = 'handleGallaryColse' v-show="showGallary" :imgs = 'imgs'></common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+    name:'DetailBanner',
+    components:{
+        CommonGallary
+    },
+    data:function(){
+        return {
+            showGallary:false,
+            imgs:[
+                  'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+                  'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg'
+                ]
+        }
+    },
+    methods:{
+        handleBannerClick:function(){
+            this.showGallary = true;
+        },
+        handleGallaryColse:function(){
+            this.showGallary = false;
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner{
+        position relative
+        overflow:hidden;
+        height :0;
+        padding-bottom :55%;
+        .banner-img{
+            width 100%
+        }
+        .banner-info{
+            display flex
+            position absolute;
+            left 0
+            right 0
+            bottom 0
+            line-height .6rem
+            color #fff
+            background-image : linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+            .banner-tittle{
+                flex:1
+                font-size .32rem
+                padding 0 .2rem
+            }
+            .banner-number{
+                padding 0 .4rem
+                height :.32rem
+                margin-top: .14rem
+                line-height :.4rem
+                border-radius 0.2rem
+                background : rgba(0,0,0, .8)
+                font-size .24rem
+                .banner-icon{
+                    font-size .24rem
+                }
+            }
+        }
+    }
+</style>
